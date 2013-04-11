3970f681f0a61f211fbf5c5e7cb0e02bf7f37205
Linus Arver <linusarver@gmail.com>
2013-04-11 01:54:16 -0700

    more intelligent uncommenting
    Use Parsec library to actually parse lines for comment characters to
    remove them, instead of blindly deleting characters/lines.

--- src/DC/Command.hs
+++ src/DC/Command.hs
@@ -7,19 +7,36 @@ import qualified Data.Text.Lazy as T
 
 import DC.Data
 import DC.Option
+import DC.Parse
 
+-- | Comment text. For single-line comments, simply prepend each line with the
+-- comment string. For multiline comments, prepend and append lines containing
+-- just the multiline comment string pairs.
 makeCmt :: Opts -> T.Text -> T.Text
-makeCmt Opts{..} str
-	| multi = flip T.append mcbn $ T.append mcan str
-	| otherwise = T.unlines . map (T.append (cmtSingle lang)) $ T.lines str
+makeCmt Opts{..} src
+	| multi = flip T.append mcbn $ T.append mcan src
+	| otherwise = T.unlines . map (T.append (cmtSingle lang)) $ T.lines src
 	where
 	(mca, mcb) = cmtMulti lang
 	mcan = T.append mca "\n"
 	mcbn = T.append mcb "\n"
 
+-- | Uncomment text. For single-line mode, remove all single-line comment
+-- characters found on each line independently of other lines. Continuous,
+-- trailing characters that are the same as the last single-line comment
+-- character are also removed (e.g., "//" is removed but so is "///" or
+-- "///////". For multiline comments, simply find and remove the mulitline
+-- comment string pairs.
 remCmt :: Opts -> T.Text -> T.Text
-remCmt Opts{..} str
-	| multi = T.unlines . init . tail $ T.lines str
-	| otherwise = T.unlines . map (T.drop n) $ T.lines str
+remCmt Opts{..} src
+	| multi = case mlineCommentExists src $ cmtMulti lang of
+		(True, removed) -> removed
+		(False, _) -> src
+	| otherwise = T.unlines . map tryRemSlineComment $ T.lines src
 	where
-	n = T.length $ cmtSingle lang
+	tryRemSlineComment :: T.Text -> T.Text
+	tryRemSlineComment rawline = if scmtExists
+		then removed
+		else rawline
+		where
+		(scmtExists, removed) = slineCommentExists rawline $ cmtSingle lang
---
+++ src/DC/Parse.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module DC.Parse where
+
+import qualified Data.Text.Lazy as T
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec.Prim
+import Text.Parsec.Text.Lazy
+
+-- | Find x in the string; skip any leading whitespace. Return the line without
+-- the (leading) comment chars.
+slineDetect :: String -> Parser String
+slineDetect x = do
+	leadSpace <- many $ oneOf " \t"
+	_ <- string leadingChars
+	_ <- many $ char trailingChar
+	rest <- manyTill anyChar $ try eof
+	return $ leadSpace ++ rest
+	where
+	leadingChars :: String
+	leadingChars = init x
+	trailingChar :: Char
+	trailingChar = last x
+
+mlineDetect :: (String, String) -> Parser String
+mlineDetect (a, b) = do
+	beg <- manyTill anyChar . lookAhead . try $ string a
+	mid <- between (string a) (string b) (manyTill anyChar . lookAhead . try $ string b)
+	end <- manyTill anyChar $ try eof
+	return $ beg ++ mid ++ end
+
+-- | Check of a single line comment exists; if so, return the uncommented version of that line.
+slineCommentExists :: T.Text -> T.Text -> (Bool, T.Text)
+slineCommentExists src slineCmtStr = case parse (slineDetect slineCmtStr') [] src of
+	Left _ -> (False, T.empty)
+	Right str -> (True, T.pack str)
+	where
+	slineCmtStr' = T.unpack slineCmtStr
+
+mlineCommentExists :: T.Text -> (T.Text, T.Text) -> (Bool, T.Text)
+mlineCommentExists src (a, b) = case parse (mlineDetect (a', b')) [] src of
+	Left _ -> (False, T.empty)
+	Right str -> (True, T.pack str)
+	where
+	(a', b') = (T.unpack a, T.unpack b)
